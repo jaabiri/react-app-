1e8599b1a261f1a9ac506e2aae11d137a790e4d0
jaabiri <jaabiri_salim@hotmail.com>
2016-03-03 15:56:03 +0100

    Life Cycle Events and Conditional Rendering

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,8 @@
+var React = require('react');
+function  ConfirmBattle(props){
+  return ( props.isLoading === true ? <p> LOADING</p>:<p> compfirm battle</p>
+
+
+  )
+}
+module.exports = ConfirmBattle;
